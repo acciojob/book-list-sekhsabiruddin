00199245cf39679bd1f1262904804c94d42dfcf2
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-02-22 16:11:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let title = document.getElementById("title").value;
+let author = document.getElementById("author").value;
+let isbn = document.getElementById("isbn").value;
+let submit = document.getElementById("submit");
+let obj ={}
+let arr=[];
+submit.addEventListner("click",function(){
+	
+obj={title,authorisbn};
+arr.push(obj);
+alert("success")
+	
+});
