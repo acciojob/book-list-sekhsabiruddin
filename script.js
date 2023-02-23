f783d541a0fe9324e4b08d3417d9b40200a448b5
sk sabiruddin <120248500+sekhsabiruddin@users.noreply.github.com>
2023-02-23 11:21:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 //your JS code here. If required.
-let title = document.getElementById("title").value;
-let author = document.getElementById("author").value;
-let isbn = document.getElementById("isbn").value;
+
+let tbody = document.getElementById("book-list");
 let submit = document.getElementById("submit");
-let obj ={}
-let arr=[];
-submit.addEventListner("click",function(){
-	
-obj={title,authorisbn};
-arr.push(obj);
-alert("success")
-	
+let obj = {};
+let arr = [];
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let isbn = document.getElementById("isbn").value;
+  let id = 0;
+  obj = { id, title, author, isbn };
+  id++;
+  arr.push(obj);
+
+  display();
 });
+function display() {
+  let nwarr = arr.map((val) => {
+    return `<tr>
+    <td>${val.title}</td>
+    <td>${val.author}</td>
+    <td>${val.isbn}</td>
+    <td class="delete" onclick="deleteRow(this)">Del</td>
+</tr>`;
+  });
+  tbody.innerHTML = nwarr.join(" ");
+}
+
+function deleteRow(r) {
+  r.parentElement.remove();
+}
